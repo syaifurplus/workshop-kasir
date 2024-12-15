f685c7922cd6d575a2ce345cd5f3f62654b29baa
Syaifur Rohman <syaifurplus@gmail.com>
2024-12-15 14:26:30 +0700

    add slug

--- database/migrations/2024_12_13_182422_create_products_table.php
+++ database/migrations/2024_12_13_182422_create_products_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('category_id')->constrained();
             $table->string('name');
+            $table->string('slug')->unique();
             $table->integer('stock')->default(0);
             $table->longText('image')->nullable();
             $table->boolean('status')->default(false);
